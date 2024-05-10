1c9619647be4bed87559dae0d3c394c4935d19c9
asishsasi1 <asishsasi0@gmail.com>
2024-05-10 10:12:11 +0530

    :completed

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Counter from './componets/Counter'
+
+function App() {
+ 
+  return (
+  <div style={{height:'100vh'}} className='d-flex justify-content-center align-items-center w-100'>
+    <Counter />
+  </div>
+  )
+}
+
+export default App
